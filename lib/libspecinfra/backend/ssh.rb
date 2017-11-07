92570ec27826a0b61e0ea18c60ffcab31a17ce2f
Gosuke Miyashita <gosukenator@gmail.com>
2017-11-08 01:38:10 +0900

    Implement SSH backend

--- lib/libspecinfra/backend/ssh.rb
+++ lib/libspecinfra/backend/ssh.rb
@@ -1,3 +1,5 @@
+require 'libspecinfra/backend/ssh_builder'
+
 module Libspecinfra::Backend
   class SSH < FFI::AutoPointer
     def self.release(ptr)
@@ -8,10 +10,26 @@ module Libspecinfra::Backend
       extend FFI::Library
       ffi_lib ['libspecinfra']
 
-      attach_function :new, :backend_ssh_new,
-                      [:string], Direct
+      def self.new(host, user, options={})
+        b = SSHBuilder::Binding.new(host).user(user)
+
+        if options[:password]
+          b = b.password(options[:password])
+        end
+
+        if options[:key_file]
+          b = b.key_file(options[:key_file])
+        end
+
+        if options[:port]
+          b = b.port(options[:port])
+        end
+
+        b.finalize()
+      end
+
       attach_function :free, :backend_ssh_free,
-                      [Direct], :void
+                      [SSH], :void
     end
   end
 end
